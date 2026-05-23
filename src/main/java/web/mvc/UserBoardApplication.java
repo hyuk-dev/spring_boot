223ca05e8fec0e209df9c8a8d588fdebb0ff3d2e
hyuk-dev <hyuk.development@gmail.com>
2026-05-23 18:01:55 +0900

    프로젝트 세팅

---
+++ src/main/java/web/mvc/UserBoardApplication.java
@@ -0,0 +1,13 @@
+package web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBoardApplication.class, args);
+    }
+
+}
